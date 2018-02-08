bc3c6cbef8eeec8146e4e919d4b91b20e0cd2145
kegriz <rafalrudko.cmzzs@gmail.com>
2018-02-08 16:49:11 +0100

    data added from json object

---
+++ src/js/data.js
@@ -0,0 +1,10 @@
+const categories = {
+  "brushes": ["Escoda", "Daley-Rowney", "Winsor&Newton"],
+  "paper": ["Saunders", "Hahnemühle", "Farbiano"],
+  "watercolor": ["White Nights", "QOR", "Schmincke", "Van Gogh", "Cotman"],
+  "oils": ["Amsterdam", "Rembrandt", "Talens", "Reeves"],
+  "drawing": ["Pencils", "Crayons", "Pens"],
+  "others": ["Easels", "Media"]
+}
+
+export const data = categories;
--- src/main.js
+++ src/main.js
@@ -1,9 +1,20 @@
 import './style/main.scss';
 import $ from 'jquery';
 import 'bootstrap';
+import { data } from './js/data.js';
 
 $( () => {
 
-  console.log($('.category'))
+    for (var i in data) {
+        var links = "";
+        for (var n in data[i]) {
+            links += "<p>"+data[i][n]+"</p>";
+        }
+
+        var element = '<div class="col-md-4"><div class="category box-shadow"><h3 class="category__title">'
+        +i+'</h3><div class="category__content container-fluid">'+links+'</div></div></div>';
+
+        $(".categories").children().append(element);
+    }
 
 });
